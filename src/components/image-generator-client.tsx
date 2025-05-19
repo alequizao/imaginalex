c37ffa5336a494fc8cc465b13308c02741271497
Alex Junior <alexjuniorcalado@gmail.com>
2025-05-19 18:00:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: React is not defined. Error source: src/components/image-generator-client.tsx (81:21) @ handleAddImageSlot
    >
    >   79 |       previewUrl: '',
    >   80 |       dataUrl: '',
    > > 81 |       fileInputRef: React.createRef<HTMLInputElement>(),
    >      |                     ^
    >   82 |     };
    >   83 |     setUploadedImageFiles(prev => [...prev, newImageFile]);
    >   84 |   };
    >
    > Call Stack
    > 13
    >
    > Show 8 ignore-listed frame(s)
    > handleAddImageSlot
    > src/components/image-generator-client.tsx (81:21)
    > button
    > <anonymous> (0:0)
    > _c
    > src/components/ui/button.tsx (46:7)
    > ImageGeneratorClient
    > src/components/image-generator-client.tsx (291:15)
    > HomePage
    > src/app/page.tsx (19:9)

--- src/components/image-generator-client.tsx
+++ src/components/image-generator-client.tsx
@@ -2,7 +2,7 @@
 // src/components/image-generator-client.tsx
 "use client";
 
-import { useState, useEffect, type ChangeEvent, type FormEvent, useRef } from 'react';
+import React, { useState, useEffect, type ChangeEvent, type FormEvent, useRef } from 'react';
 import Image from 'next/image';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
